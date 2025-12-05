26d3a5a8749956c32b79f508aa44a40c0bcaa150
Joaaalves <contato@joao-alves.com>
2025-12-05 17:57:23 -0300

    feat: Adds ValueObject base class

---
+++ src/Joaoaalves.DDD/Common/IgnoreMemberAttribute.cs
@@ -0,0 +1,10 @@
+namespace Joaoaalves.DDD.Common
+{
+    /// <summary>
+    /// Attribute used to exclude a field or property from equality and hash code calculations in value objects.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class IgnoreMemberAttribute : Attribute
+    {
+    }
+}
